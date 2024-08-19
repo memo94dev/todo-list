8a2505257f0fd6e8be3befd2cc9cf9511a97eec8
Guillermo Barrientos Home <guille.work.94@gmail.com>
2024-08-19 16:15:35 -0400

    Dando formato a mis archivos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
-const ul = document.querySelector("ul");
+const ol = document.querySelector("ol");
 const empty = document.querySelector(".empty");
 
 addBtn.addEventListener("click", (e) => {
@@ -17,7 +17,7 @@ addBtn.addEventListener("click", (e) => {
 
     li.appendChild(p);
     li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
+    ol.appendChild(li);
 
     input.value = "";
     empty.style.display = "none";
@@ -32,7 +32,7 @@ function addDeleteBtn() {
 
   deleteBtn.addEventListener("click", (e) => {
     const item = e.target.parentElement;
-    ul.removeChild(item);
+    ol.removeChild(item);
 
     const items = document.querySelectorAll("li");
 
@@ -63,4 +63,23 @@ function temados(){
 function tema(){
   var cuerpo = document.querySelector("body");
   cuerpo.style.background = "var(--clr-fondo)";
+}
+
+// Función para descargar la lista generada en formato .txt
+
+function descargar() {
+  // Supongamos que tienes un elemento HTML con el contenido que deseas exportar
+  
+  const contenidoHTML = document.getElementById('contenido').innerText;
+
+  // Crea un enlace de descarga para el archivo TXT
+  const enlaceDescarga = document.createElement('a');
+  enlaceDescarga.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(contenidoHTML);
+  enlaceDescarga.download = 'Mi Lista.txt';
+  //   enlaceDescarga.tittle = 'Descargar TXT';
+
+  // Agrega el enlace al DOM
+  document.body.appendChild(enlaceDescarga);
+  enlaceDescarga.click();
+  document.body.removeChild(enlaceDescarga);
 }
